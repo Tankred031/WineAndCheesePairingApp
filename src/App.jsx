394322a39dd5a0e5f59430cecc5ee129b0fb85f6
Tankred031 <tankred.kralj@gmail.com>
2026-03-18 17:08:38 +0100

    vracanje na dobro

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Container } from 'react-bootstrap'
 import Izbornik from './components/Izbornik'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Uparivanje from './pages/Uparivanje'
 import { RouteNames } from './constants'
 import VinaPregled from './pages/vina/VinaPregled'
 import SireviPregled from './pages/sirevi/SireviPregled'
-import UparivanjeService from './services/uparivanje/UparivanjeService'
 
 function App() {
 
@@ -20,7 +20,7 @@ function App() {
         <Route path={RouteNames.HOME} element={<Home />} />
         <Route path={RouteNames.VINA_PREGLED} element={<VinaPregled />} />
         <Route path={RouteNames.SIREVI_PREGLED} element={<SireviPregled />} />
-        <Route path={RouteNames.UPARIVANJE} element={<UparivanjeService />} />
+        <Route path={RouteNames.UPARIVANJE} element={<Uparivanje />} />
       </Routes>
       <hr />
       &copy; Tankred
---
+++ src/pages/Uparivanje.jsx
@@ -0,0 +1,7 @@
+export default function Uparivanje(){
+    return(
+        <>
+        Ovdje će uparivanje
+        </>
+    )
+}
