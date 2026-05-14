8dbed04fe5ceded3eb92503e0bd36def29abc710
Tankred031 <tankred.kralj@gmail.com>
2026-05-14 18:40:54 +0200

    Update DodajZanimljivost.jsx

--- src/pages/zanimljivosti/DodajZanimljivost.jsx
+++ src/pages/zanimljivosti/DodajZanimljivost.jsx
@@ -109,10 +109,25 @@ export default function DodajZanimljivost() {
                         </Form.Control.Feedback>
                     </Form.Group>
 
-                    <button className="btn btn-warning w-100">
-                        Dodaj karticu
-                    </button>
+                    <div className="d-flex gap-2 mt-3">
 
+                        <button
+                            type="button"
+                            className="btn btn-danger w-100"
+                            onClick={() => navigate(RouteNames.ZANIMLJIVOSTI)}
+                        >
+                            Odustani
+                        </button>
+
+
+                        <button
+                            type="submit"
+                            className="btn btn-success w-100"
+                        >
+                            Dodaj karticu
+                        </button>
+
+                    </div>
                 </Form>
             </div>
         </div>
